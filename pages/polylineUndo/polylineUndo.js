4e45de662476ca0c05bf6d2f3f917d0c4aaaa3cf
CamilleKerherve <camille.kerherve@etud.univ-jfc.fr>
2024-10-16 10:16:26 +0200

    UndoManager

--- pages/polylineUndo/polylineUndo.js
+++ pages/polylineUndo/polylineUndo.js
@@ -18,6 +18,29 @@ stage.add(temporaire);
 const MAX_POINTS = 10;
 let polyline // La polyline en cours de construction;
 
+class UndoManager {
+
+    constructor() {
+        this.redoStack = new Stack()
+        this.undoStack = new Stack()
+    }
+    execute(commande) {
+        commande.execute()
+        this.undoStack.push(commande)
+    }
+
+    undo() {
+            this.element = this.undoStack.pop()
+            this.element.undo()
+            this.redoStack.push(this.element)
+    }
+    redo() {
+            this.element = this.redoStack.pop()
+            this.element.execute()
+            this.undoStack.push(this.element)
+    }
+}
+
 const polylineMachine = createMachine(
     {
         /** @xstate-layout N4IgpgJg5mDOIC5QAcD2AbAngGQJYDswA6XCdMAYgFkB5AVQGUBRAYWwEkWBpAbQAYAuohSpYuAC65U+YSAAeiAIwAWAGxEArAGYAHDo0AmA3x2qNqxRoA0ITEoCciosoDsRg-Zf6DWg4pcAvgE2aFh4hETSYAAKqATi1PTMbJy8grJoYpLSsgoIFvZEOpbKij5l9jou9jZ2CIqOzm4+Ospa1YZ8qkEhGDgExFGx8YmMTLQAakz8QkggmRJSMnN5rlpE9ho69j5aWpVtLta2SnxOBjpaDRcWygZqOj3zfeGDhMP4CUywAMYAhsgwDMMqJFjkVkpVAZnNt2mV9tVfFpaohfHwigYNG0NOZVEcdAYnqF+hEALZ-fCYD7iWCjZIcbjAuYLbLLUB5RR8FzrcxaZR8YwXFyuFEIAxeIgubmqPH2eyuExtIkvAZEcmU6m02hjFKMxSzERZJa5U6qHSadp85RYy6KKqirR45xaPhdUqKO1mZTKsKq9VUuKfLVJcY0KZMw1gtnyJRlDREGVQtyGLHWlyiznnW07Uwqe6BYLPX1kikB+K077-QER55G8HsxA6AWaLmeHFVLFQh2FKoGKHy-ZaDRlAu9YvEf2aihMT5gABONZZxohCB2PalRiHpj5JlFAFpOc4PUd2splJcXDLHoXia81aWpwAhP4-ADWsGQL6B6WZoNZJoQFxFHUNp2kuGVrTxZQHWMDEsSHXF8UJG8VRLDVAxpChnzfD8vx4fUQTraNVlUZQikqTo7XsLlqhg9ECXgnEZXxRQtCCQt8FQCA4BBcdCKjAC9zUI9pVUAUvDRZETgQPdPHIvhHC8M12guZCxxJYhSHIfj-xXJwtj7PZPD8WUzR0UU-DIzFTE8UoqmAvkfQ0yJ3gwnTlwbBBTyIfZzz4DRqIJPgrgslQiGs1RbLtIDVEclDx3vdDy3c+sY1XS5JQ0BT2li6zzOkyyE0RD1gv2Kp5XYgIgA */
@@ -120,8 +143,8 @@ const polylineMachine = createMachine(
                 polyline.stroke("black"); // On change la couleur
                 // On sauvegarde la polyline dans la couche de dessin
                 const commande = new ConcreteCommand(polyline, dessin)
-                commande.execute()
-              
+                //commande.execute()
+                undoManager = execute(commande);
                 
             },
             addPoint: (context, event) => {
@@ -156,7 +179,7 @@ const polylineMachine = createMachine(
     }
 );
 
-//const undoManager=new UndoManager()
+let undoManager=new UndoManager()
 
 const polylineService = interpret(polylineMachine)
     .onTransition((state) => {
@@ -180,14 +203,13 @@ window.addEventListener("keydown", (event) => {
 // bouton Undo
 const undoButton = document.getElementById("undo");
 undoButton.addEventListener("click", () => {
-    //undoManager.undo()
+    undoManager.undo()
 });
 
 
 class Command{
     execute() {
     }
-
     undo() {
     }
 }
@@ -206,31 +228,3 @@ class ConcreteCommand extends Command{
     }
 }
 
-class UndoManager {
-
-    constructor() {
-        this.redoStack = new Stack()
-        this.undoStack = new Stack()
-    }
-
-
-    execute(commande) {
-        commande.execute()
-        this.undoStack.push(commande)
-    }
-
-    undo() {
-            this.element = this.undoStack.pop()
-            this.element.undo()
-            this.redoStack.push(this.element)
-        
-    }
-
-    redo() {
-      
-            this.element = this.redoStack.pop()
-            this.element.execute()
-            this.undoStack.push(this.element)
-        
-    }
-}
